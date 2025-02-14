4c36e7d8d9f5e63f25ee72ef83b4bec6fdca4574
inkrodr22 <inaki.rodriguez.14@gmail.com>
2025-02-14 11:27:27 -0600

    Ajuste de conexion a bd

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,36 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
     charset: 'utf8mb4'
 
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
     if (err) {
         console.error('Error conectando a MySQL:', err);
         return;
     }
     console.log('Conectado a MySQL');
+    connection.release();
 });
 
-module.exports = connection;
+setInterval(() => {
+    pool.query('SELECT 1', (err) => {
+        if (err) {
+            console.error('Error en keep-alive query:', err);
+        } else {
+            console.log('Keep-alive query ejecutada.');
+        }
+    });
+}, 3600000);
+
+
+module.exports = pool;
